966c4ba6397742057411acd3d1ee7771e2c9a6fb
Thiago Martins <thinomar@gmail.com>
2020-10-29 03:25:16 +0000

    Implementação de busca de vídeos e integração com player

---
+++ public/bandersnatch/src/videoPlayer.js
@@ -0,0 +1,78 @@
+class VideoMediaPlayer {
+    constructor({ manifestJSON, network }) {
+        this.manifestJSON = manifestJSON;
+        this.network = network;
+        this.videoElement = null;
+        this.sourceBuffer = null;
+        this.selected = {};
+        this.videoDuration = 0;
+    }
+
+    initializeCodec() {
+        this.videoElement = document.getElementById("vid");
+        const mediaSourceSupported = !!window.MediaSource;
+        
+        if (!mediaSourceSupported) {
+            alert('Seu browser ou sistema não tem suporte a MSE!')
+            return;
+        }
+
+        const codecSupported = MediaSource.isTypeSupported(this.manifestJSON.codec);
+
+        if(!codecSupported) {
+            alert(`Seu browser não suporta o codec: ${this.manifestJSON.codec}`);
+            return;
+        }
+
+        const mediaSource = new MediaSource();
+        this.videoElement.src = URL.createObjectURL(mediaSource);
+
+        mediaSource.addEventListener('sourceopen', this.sourceOpenWrapper(mediaSource));
+    }
+
+    sourceOpenWrapper(mediaSource) {
+        return async (_) => {
+            this.sourceBuffer = mediaSource.addSourceBuffer(this.manifestJSON.codec);
+            const selected = this.selected = this.manifestJSON.intro;
+
+            mediaSource.duration = this.videoDuration;
+            await this.fileDownload(selected.url);
+        };
+    }
+
+    async fileDownload(url) {
+        const prepareUrl = { 
+            url, 
+            fileResolution: 360, 
+            fileResolutionTag: this.manifestJSON.fileResolutionTag, 
+            hostTag: this.manifestJSON.hostTag
+        };
+
+        const finalURL = this.network.parseManifestURL(prepareUrl);
+        this.setVideoPlayerDuration(finalURL);
+        const data = await this.network.fetchFile(finalURL);
+        this.processBufferSegments(data);
+    }
+
+    setVideoPlayerDuration(finalURL) {
+        const bars = finalURL.split('/');
+        const [ name, videoDuration ] = bars[bars.length -1].split('-');
+        this.videoDuration += videoDuration;
+    }
+
+    async processBufferSegments(allSegments) {
+        const sourceBuffer = this.sourceBuffer;
+        sourceBuffer.appendBuffer(allSegments);
+
+        return new Promise((resolve, reject) => {
+            const updateEnd = (_) => {
+                sourceBuffer.removeEventListener('updateend', updateEnd);
+                sourceBuffer.timestampOffset = this.videoDuration;
+                return resolve();
+            }
+
+            sourceBuffer.addEventListener('updateend', updateEnd);
+            sourceBuffer.addEventListener('error', () => reject);
+        });
+    }
+}
